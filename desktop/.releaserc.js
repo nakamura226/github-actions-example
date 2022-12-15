58912ed8b6094a457a2d57d0dd690527ebeb738c
underground <nacanori@gmail.com>
2022-12-15 12:41:40 +0900

    :sparkles: (desktop): 元に戻す

--- desktop/.releaserc.js
+++ desktop/.releaserc.js
@@ -4,15 +4,17 @@ module.exports = {
   branches: ["main"],
   tagFormat: "releases/uploader-ui/v${version}",
   plugins: [
-    ["semantic-release-gitmoji", {
-      releaseRules: {
-        // major: [💥 :boom:]
-        major: gitmojis.filter(({ semver }) => semver === 'major').map(({ code }) => code),
-        // minor: [✨ :sparkles:]
-        minor: gitmojis.filter(({ semver }) => semver === 'minor').map(({ code }) => code),
-        // patch: [⚡️ :zap:, 🐛 :bug:, 🚑️ :ambulance:, 💄 :lipstick:, 🔒️ :lock:, ⬇️ :arrow_down:, ⬆️ :arrow_up:, 📌 :pushpin:, 📈 :chart_with_upwards_trend:, ➕ :heavy_plus_sign:, ➖ :heavy_minus_sign:, 🔧 :wrench:, 🌐 :globe_with_meridians:, ✏️ :pencil2:, ⏪️ :rewind:, 📦️ :package:, 👽️ :alien:, 🍱 :bento:, ♿️ :wheelchair:, 💬 :speech_balloon:, 🗃️ :card_file_box:, 🚸 :children_crossing:, 📱 :iphone:, 🥚 :egg:, ⚗️ :alembic:, 🔍️ :mag:, 🏷️ :label:, 🚩 :triangular_flag_on_post:, 🥅 :goal_net:, 💫 :dizzy:, 🗑️ :wastebasket:, 🛂 :passport_control:, 🩹 :adhesive_bandage:, 👔 :necktie:]
-        patch: gitmojis.filter(({ semver }) => semver === 'patch').map(({ code }) => code),
-      }
+    ["@semantic-release/commit-analyzer", {
+      preset: "atom",
+      // major: [💥 :boom:]
+      // minor: [✨ :sparkles:]
+      // patch: [⚡️ :zap:, 🐛 :bug:, 🚑️ :ambulance:, 💄 :lipstick:, 🔒️ :lock:, ⬇️ :arrow_down:, ⬆️ :arrow_up:, 📌 :pushpin:, 📈 :chart_with_upwards_trend:, ➕ :heavy_plus_sign:, ➖ :heavy_minus_sign:, 🔧 :wrench:, 🌐 :globe_with_meridians:, ✏️ :pencil2:, ⏪️ :rewind:, 📦️ :package:, 👽️ :alien:, 🍱 :bento:, ♿️ :wheelchair:, 💬 :speech_balloon:, 🗃️ :card_file_box:, 🚸 :children_crossing:, 📱 :iphone:, 🥚 :egg:, ⚗️ :alembic:, 🔍️ :mag:, 🏷️ :label:, 🚩 :triangular_flag_on_post:, 🥅 :goal_net:, 💫 :dizzy:, 🗑️ :wastebasket:, 🛂 :passport_control:, 🩹 :adhesive_bandage:, 👔 :necktie:]
+      releaseRules: gitmojis.reduce((acc, { semver, emoji }) => {
+        if (semver) {
+          acc.push({ emoji, scope: "desktop", release: semver });
+        }
+        return acc;
+      }, []),
     }],
     "@semantic-release/release-notes-generator",
     ["@semantic-release/npm", {
