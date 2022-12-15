cc25f470f480a5acd66e84f24af5f200da152e95
underground <nacanori@gmail.com>
2022-12-15 13:21:08 +0900

    :sparkles: (desktop): 絵文字コードでも検知するようにしてみる

--- desktop/.releaserc.js
+++ desktop/.releaserc.js
@@ -8,9 +8,10 @@ module.exports = {
       // major: [💥 :boom:]
       // minor: [✨ :sparkles:]
       // patch: [⚡️ :zap:, 🐛 :bug:, 🚑️ :ambulance:, 💄 :lipstick:, 🔒️ :lock:, ⬇️ :arrow_down:, ⬆️ :arrow_up:, 📌 :pushpin:, 📈 :chart_with_upwards_trend:, ➕ :heavy_plus_sign:, ➖ :heavy_minus_sign:, 🔧 :wrench:, 🌐 :globe_with_meridians:, ✏️ :pencil2:, ⏪️ :rewind:, 📦️ :package:, 👽️ :alien:, 🍱 :bento:, ♿️ :wheelchair:, 💬 :speech_balloon:, 🗃️ :card_file_box:, 🚸 :children_crossing:, 📱 :iphone:, 🥚 :egg:, ⚗️ :alembic:, 🔍️ :mag:, 🏷️ :label:, 🚩 :triangular_flag_on_post:, 🥅 :goal_net:, 💫 :dizzy:, 🗑️ :wastebasket:, 🛂 :passport_control:, 🩹 :adhesive_bandage:, 👔 :necktie:]
-      releaseRules: gitmojis.reduce((acc, { semver, emoji }) => {
+      releaseRules: gitmojis.reduce((acc, { semver, code, emoji }) => {
         if (semver) {
           acc.push({ emoji, scope: "desktop", release: semver });
+          acc.push({ emoji: code, scope: "desktop", release: semver });
         }
         return acc;
       }, []),
