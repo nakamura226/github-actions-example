87b002935c375d74adb2302d3da677acecd9bb40
underground <nacanori@gmail.com>
2022-12-15 12:55:29 +0900

    :bug: (desktop): preset: atom をやめる

--- desktop/.releaserc.js
+++ desktop/.releaserc.js
@@ -5,7 +5,6 @@ module.exports = {
   tagFormat: "releases/uploader-ui/v${version}",
   plugins: [
     ["@semantic-release/commit-analyzer", {
-      preset: "atom",
       // major: [💥 :boom:]
       // minor: [✨ :sparkles:]
       // patch: [⚡️ :zap:, 🐛 :bug:, 🚑️ :ambulance:, 💄 :lipstick:, 🔒️ :lock:, ⬇️ :arrow_down:, ⬆️ :arrow_up:, 📌 :pushpin:, 📈 :chart_with_upwards_trend:, ➕ :heavy_plus_sign:, ➖ :heavy_minus_sign:, 🔧 :wrench:, 🌐 :globe_with_meridians:, ✏️ :pencil2:, ⏪️ :rewind:, 📦️ :package:, 👽️ :alien:, 🍱 :bento:, ♿️ :wheelchair:, 💬 :speech_balloon:, 🗃️ :card_file_box:, 🚸 :children_crossing:, 📱 :iphone:, 🥚 :egg:, ⚗️ :alembic:, 🔍️ :mag:, 🏷️ :label:, 🚩 :triangular_flag_on_post:, 🥅 :goal_net:, 💫 :dizzy:, 🗑️ :wastebasket:, 🛂 :passport_control:, 🩹 :adhesive_bandage:, 👔 :necktie:]
