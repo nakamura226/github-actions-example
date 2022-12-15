473194fce734eb27c844466f3cce10ad99d3d278
underground <nacanori@gmail.com>
2022-12-15 12:13:53 +0900

    :sparkles: semantic-release-gitmoji でリリースの確認

--- desktop/.releaserc.js
+++ desktop/.releaserc.js
@@ -4,13 +4,15 @@ module.exports = {
   branches: ["main"],
   tagFormat: "releases/uploader-ui/v${version}",
   plugins: [
-    ["@semantic-release/commit-analyzer", {
-      releaseRules: gitmojis.reduce((acc, { semver, code }) => {
-        if (semver) {
-          acc.push({ type: code, scope: "ui", release: semver });
-        }
-        return acc;
-      }, []),
+    ["semantic-release-gitmoji", {
+      releaseRules: {
+        // major: [💥 :boom:]
+        major: gitmojis.filter(({ semver }) => semver === 'major').map(({ code }) => code),
+        // minor: [✨ :sparkles:]
+        minor: gitmojis.filter(({ semver }) => semver === 'minor').map(({ code }) => code),
+        // patch: [⚡️ :zap:, 🐛 :bug:, 🚑️ :ambulance:, 💄 :lipstick:, 🔒️ :lock:, ⬇️ :arrow_down:, ⬆️ :arrow_up:, 📌 :pushpin:, 📈 :chart_with_upwards_trend:, ➕ :heavy_plus_sign:, ➖ :heavy_minus_sign:, 🔧 :wrench:, 🌐 :globe_with_meridians:, ✏️ :pencil2:, ⏪️ :rewind:, 📦️ :package:, 👽️ :alien:, 🍱 :bento:, ♿️ :wheelchair:, 💬 :speech_balloon:, 🗃️ :card_file_box:, 🚸 :children_crossing:, 📱 :iphone:, 🥚 :egg:, ⚗️ :alembic:, 🔍️ :mag:, 🏷️ :label:, 🚩 :triangular_flag_on_post:, 🥅 :goal_net:, 💫 :dizzy:, 🗑️ :wastebasket:, 🛂 :passport_control:, 🩹 :adhesive_bandage:, 👔 :necktie:]
+        patch: gitmojis.filter(({ semver }) => semver === 'patch').map(({ code }) => code),
+      }
     }],
     "@semantic-release/release-notes-generator",
     ["@semantic-release/npm", {
