65544001098e0072bb0845b9adb94bd02c4ad3a8
underground <nacanori@gmail.com>
2022-12-16 14:54:01 +0900

    ci(desktop): windows makeできるように設定追加

--- desktop/forge.config.ts
+++ desktop/forge.config.ts
@@ -1,8 +1,6 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
 import { mainConfig } from './webpack.main.config';
@@ -11,7 +9,13 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {},
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [
+    new MakerSquirrel({
+      certificateFile: process.env.WINDOWS_PFX_FILE,
+      certificatePassword: process.env.WINDOWS_PFX_PASSWORD,
+    }),
+    new MakerZIP({}, ['win32']),
+  ],
   plugins: [
     new WebpackPlugin({
       mainConfig,
